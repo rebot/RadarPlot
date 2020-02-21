0080961e43a2f544a9e3eb11bf2b2703b16b42de
Gilles Trenson <rebot@users.noreply.github.com>
2020-02-21 13:59:26 +0100

    not working - WIP

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+# In[0]: Imports
+
 import os
 from pyx import *
 import numpy as np
 import pandas as pd
+import itertools as it
 from pylatexenc.latexencode import unicode_to_latex
 
 # In[0]: Default settings
@@ -11,8 +14,8 @@ text.set(text.LatexEngine)
 text.preamble(
     r'\usepackage[scaled]{helvet}\renewcommand\familydefault{\sfdefault} ')
 
-if not os.path.exists('src/'):
-    os.mkdir('src/')
+if not os.path.exists('export/'):
+    os.mkdir('export/')
 
 
 # In[1]: Make the generateRadarPlot function
@@ -20,26 +23,29 @@ if not os.path.exists('src/'):
 class RadarPlot(object):
 
     s = 0.5  # Scale
-    r = 9   # Radius
-    g = 10  # Gridlines
+    r = 5   # Radius
+    g = 5  # Gridlines
+    # Define default colors
+    colors = ['#9CD5E7', '#E1D2C6', '#03A698']
 
-    def __init__(self, prov, woon, kern):
-        self.p = prov
-        self.w = woon
-        self.k = kern
-        # Number of items
-        self.n = prov.shape[0]
-        self.t = np.linspace(0, 2 * np.pi, num=self.n,
-                             endpoint=False) + np.pi / 2
+    def __init__(self):
+        # Maak dataBin aan
+        self.dataBin = []
         # Generate canvas
         self.c = canvas.canvas()
 
     def generateCanvas(self):
 
+        # Maak rechte lijnen
         for a in self.t:
             self.c.stroke(path.path(path.moveto(0, 0), path.lineto(self.s * self.r * np.cos(
                 a), self.s * self.r * np.sin(a))), [style.linestyle.solid, color.gray(0.9)])
 
+        # Maak de rand aan
+        r = np.array(self.g) * self.s
+        self.c.stroke(path.path(path.moveto(0, r[0]), *[path.lineto(r_ * np.cos(a), r_ * np.sin(a)) for r_, a in zip(r[1:], self.t[1:])], path.closepath()), [style.linestyle.solid, color.gray(0.9)])
+
+        # Maak het grid aan
         grid = range(1, self.g)
         for r in grid:
             clr = [style.linestyle.solid, color.gray(0.1)]
@@ -49,7 +55,9 @@ class RadarPlot(object):
             self.c.stroke(path.path(path.moveto(0, self.s * r), *[path.lineto(self.s * r * np.cos(
                 a), self.s * r * np.sin(a)) for a in self.t], path.closepath()), clr)
 
-        for t, a in zip(self.p.index, self.t):
+        # Voeg titels toe
+        titels = self.dataBin[0]['data'].columns
+        for t, a in zip(titels, self.t):
             # Determine horizontal alignment
             halign = [text.halign.boxcenter, text.halign.flushcenter]
             if int(np.cos(a) * 10) < 0:
@@ -63,19 +71,30 @@ class RadarPlot(object):
     def generateRadarPlot(self):
         # Generate the plot
         self.generateCanvas()
-        # Definieer de kleuren
-        colors = ['#9CD5E7', '#E1D2C6', '#03A698']
 
-        for d, c in zip([self.p, self.w, self.k], colors):
+        # Teken de lijnen
+        for i in self.dataBin:
+            d = i['data']
+            c = i['color']
             r = np.array(d) * self.s
             self.c.stroke(path.path(path.moveto(0, r[0]), *[path.lineto(r_ * np.cos(a), r_ * np.sin(a)) for r_, a in zip(r[1:], self.t[1:])], path.closepath()), [
                           color.rgbfromhexstring(c), style.linewidth(0.06), deco.filled([color.rgbfromhexstring(c), color.transparency(0.9)])])
 
-    def addLegend(self, prov, woon, kern):
-        # Definieer kleuren
-        colors = ['#9CD5E7', '#E1D2C6', '#03A698']
-        names = ['Provincie', 'Woongebied', 'Kern']
+    def addLayer(self, data, name='default', color=None):
+        # Definieer het aantal items 
+        if len(self.dataBin) ==0:  
+            self.n = data.shape[1]
+            self.t = np.linspace(0, 2 * np.pi, num=self.n,
+                                endpoint=False) + np.pi / 2
+        # Voeg gegevens toe
+        self.dataBin.append({
+            'name': name,
+            'data': data,
+            'color': it.cycle(self.colors) if color == None else color
+        })
 
+    def addLegend(self):
+        # Definieer kleuren
         for i, pre, name, c in zip(range(3), names, [prov, woon, kern], colors):
             x = - self.s * self.r * 2.5
             y =  - self.s * (self.r + 3 - (i/2)*2)
@@ -84,28 +103,22 @@ class RadarPlot(object):
 
     def save(self, name='test'):
         # Schrijf een PDF file weg
-        self.generateRadarPlot()
+        self.generateCanvas()
         self.c.writePDFfile(os.path.join('src', name))
 
 
 # In[2]: Load the data
 
-df = pd.read_excel('data.xlsx')
+df = pd.read_excel('20200214kernstudie.xlsx')
 
-# Loop over alle provincies
-provincies = df.groupby('Provincie')
-for provincie, data in provincies:
-    prov_gem = data.mean()
-    # Loop over alle woongebieden
-    woongebieden = data.groupby('Woongebied')
-    for woongebied, data in woongebieden:
-        woon_gem = data.mean()
-        # Loop over alle kernen
-        kernen = data.groupby('Kern')
-        for kern, data in kernen:
-            kern_gem = data.mean()
-            # Genereer een plot voor elke kern
-            plot = RadarPlot(prov_gem, woon_gem, kern_gem)
-            plot.addLegend(provincie.capitalize(), woongebied.capitalize(), kern.capitalize())
-            plot.save(provincie.lower() + '_' +
-                      woongebied.lower() + '_' + kern.lower())
+# In[3]: Loop over alle provincies
+ids = df.groupby('ID')
+for i, data in ids:
+    if i > 2:
+        exit
+    # Loop over alle waardes
+    plot = RadarPlot()
+    plot.addLayer(data.iloc[:,1:], name='kernstudie', color='#B1DFD5')
+    plot.save(f'{i}')
+
+# %%
