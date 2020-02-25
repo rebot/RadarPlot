4d66062dcd1146929353724ac60e7a665b200b7a
B0hr <trenson.gilles@gmail.com>
2020-02-26 00:05:59 +0100

    klasse aangepast - bug verholpen - data in te lezen als DataSeries - export naar .svg

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-# In[0]: Imports
+# In[0]: Importeer de dependencies
 
 import os
 from pyx import *
@@ -7,7 +7,7 @@ import pandas as pd
 import itertools as it
 from pylatexenc.latexencode import unicode_to_latex
 
-# In[0]: Default settings
+# In[1]: Standaard instellingen
 
 unit.set(wscale=2)
 text.set(text.LatexEngine)
@@ -17,108 +17,184 @@ text.preamble(
 if not os.path.exists('export/'):
     os.mkdir('export/')
 
-
-# In[1]: Make the generateRadarPlot function
+# In[2]: RadarPlot klasse
 
 class RadarPlot(object):
 
-    s = 0.5  # Scale
-    r = 5   # Radius
-    g = 5  # Gridlines
-    # Define default colors
-    colors = ['#9CD5E7', '#E1D2C6', '#03A698']
+    s = 0.5     # Schaal
+    r = 5       # Straal
+    g = None    # Aantal gridlijnen
+    n = None    # Aantal items = f(data)
+    t = None    # Array met de hoeken
 
-    def __init__(self):
-        # Maak dataBin aan
-        self.dataBin = []
-        # Generate canvas
+    # Definieer de standaard kleuren
+    colors = it.cycle(['#B1DFD5', '#E1D2C6', '#03A698'])
+
+    def __init__(self, **kwargs):
+        # Vul parameters aan
+        self.s = kwargs.get('schaal', self.s)
+        self.r = kwargs.get('straal', self.r)
+        self.g = kwargs.get('gridlines', None)
+        self.n = kwargs.get('items', None)
+        # Als het aantal items gedefinieerd is, maak dan een 
+        # lijst met de verschillende hoeken aan
+        if self.n != None:
+            # Start bovenaan, op de hoek pi/2 of 90°
+            self.t = np.linspace(0, 2 * np.pi, num=self.n,
+                                endpoint=False) + np.pi / 2
+        # Maak layers aan
+        self.layers = []
+        # Maak context aan - hierop wordt getekend
         self.c = canvas.canvas()
 
     def generateCanvas(self):
-
-        # Maak rechte lijnen
+        # 1. Maak rechte lijnen
         for a in self.t:
-            self.c.stroke(path.path(path.moveto(0, 0), path.lineto(self.s * self.r * np.cos(
-                a), self.s * self.r * np.sin(a))), [style.linestyle.solid, color.gray(0.9)])
-
-        # Maak de rand aan
-        r = np.array(self.g) * self.s
-        self.c.stroke(path.path(path.moveto(0, r[0]), *[path.lineto(r_ * np.cos(a), r_ * np.sin(a)) for r_, a in zip(r[1:], self.t[1:])], path.closepath()), [style.linestyle.solid, color.gray(0.9)])
-
-        # Maak het grid aan
-        grid = range(1, self.g)
-        for r in grid:
-            clr = [style.linestyle.solid, color.gray(0.1)]
-            if r < 9:
-                clr = [style.linestyle(
-                    style.linecap.round, style.dash([0, 2])), color.gray(0.3)]
-            self.c.stroke(path.path(path.moveto(0, self.s * r), *[path.lineto(self.s * r * np.cos(
-                a), self.s * r * np.sin(a)) for a in self.t], path.closepath()), clr)
-
-        # Voeg titels toe
-        titels = self.dataBin[0]['data'].columns
-        for t, a in zip(titels, self.t):
-            # Determine horizontal alignment
+            self.c.stroke(
+                path.path(
+                    path.moveto(0, 0),
+                    path.lineto(self.s * self.r * np.cos(a),
+                    self.s * self.r * np.sin(a))
+                ), 
+                [style.linestyle.solid, color.gray(0.9)]
+            )
+        # 2. Maak het grid aan - aantal lijnen = gridlines - 1 (buitenlijn)
+        for r in np.linspace(0, self.r, self.g + 1):
+            clr = [style.linestyle.solid, color.gray(0.9)]
+            if r < self.r:
+                clr = [
+                    # Stippellijn
+                    style.linestyle(
+                        style.linecap.round, 
+                        style.dash([0, 2])
+                    ), 
+                    # Donker grijze kleur
+                    color.gray(0.3)
+                ]
+            self.c.stroke(
+                path.path(
+                    path.moveto(0, self.s * r),
+                    *[path.lineto(self.s * r * np.cos(a), self.s * r * np.sin(a)) for a in self.t],
+                    path.closepath()
+                ), 
+                # Geef de stijl mee = f(r)
+                clr
+            )
+        # 3. Voeg titels toe - Index van pandas Series
+        titels = self.layers[0]['data'].index
+        for t, a in zip(titels[:self.n], self.t):
+            # Bepaal de horizontale uitlijning
             halign = [text.halign.boxcenter, text.halign.flushcenter]
             if int(np.cos(a) * 10) < 0:
                 halign = [text.halign.boxright, text.halign.flushright]
             elif int(np.cos(a) * 10) > 0:
                 halign = [text.halign.boxleft, text.halign.flushleft]
-            # Plot the text
-            self.c.text(self.s * (self.r + 1) * np.cos(a), self.s * (self.r + 1) * np.sin(a),
-                        unicode_to_latex(t), [text.parbox(5), *halign, text.valign.middle, color.gray(0.3)])
-
+            # Voeg de tekst toe aan de context
+            self.c.text(
+                self.s * (self.r + 1) * np.cos(a),
+                self.s * (self.r + 1) * np.sin(a),
+                # Trancodeer de text naar Latex code
+                unicode_to_latex(t), 
+                # Geef mee hoe de tekstbox gedefinieerd is
+                [text.parbox(5), *halign, text.valign.middle, color.gray(0.3)]
+            )
+        
     def generateRadarPlot(self):
-        # Generate the plot
+        # Maak een lege canvas aan
         self.generateCanvas()
-
-        # Teken de lijnen
-        for i in self.dataBin:
-            d = i['data']
+        # Itereer over de lagen en voeg hen toe
+        for i in self.layers:
+            d = i['data'].values
             c = i['color']
-            r = np.array(d) * self.s
-            self.c.stroke(path.path(path.moveto(0, r[0]), *[path.lineto(r_ * np.cos(a), r_ * np.sin(a)) for r_, a in zip(r[1:], self.t[1:])], path.closepath()), [
-                          color.rgbfromhexstring(c), style.linewidth(0.06), deco.filled([color.rgbfromhexstring(c), color.transparency(0.9)])])
+            r = d * self.s
+            self.c.stroke(
+                path.path(
+                    path.moveto(0, r[0]), 
+                    *[path.lineto(r_ * np.cos(a), r_ * np.sin(a)) for r_, a in zip(r[1:], self.t[1:])], 
+                    path.closepath()
+                ), 
+                [
+                    # Zet het kleur om naar een rgb waarde
+                    color.rgbfromhexstring(c), 
+                    style.linewidth(self.s * 0.06), 
+                    # Hieronder wordt de vulkleur gedefinieerd - transparency = 90%
+                    deco.filled([color.rgbfromhexstring(c), color.transparency(0.9)])
+                ]
+            )
 
     def addLayer(self, data, name='default', color=None):
         # Definieer het aantal items 
-        if len(self.dataBin) ==0:  
+        if self.n == None:  
             self.n = data.shape[1]
             self.t = np.linspace(0, 2 * np.pi, num=self.n,
                                 endpoint=False) + np.pi / 2
         # Voeg gegevens toe
-        self.dataBin.append({
+        self.layers.append({
             'name': name,
             'data': data,
-            'color': it.cycle(self.colors) if color == None else color
+            'color': next(self.colors) if color == None else color
         })
 
     def addLegend(self):
         # Definieer kleuren
-        for i, pre, name, c in zip(range(3), names, [prov, woon, kern], colors):
+        j = 0
+        for layer in self.layers:
+            # Als geen naam toegekend is, doe verder
+            if layer['name'] == 'default':
+                continue 
+            # Positie = f(huidige index)
             x = - self.s * self.r * 2.5
-            y =  - self.s * (self.r + 3 - (i/2)*2)
-            self.c.stroke(path.path(path.moveto(x,y), path.rlineto(1, 0)), [color.rgbfromhexstring(c), style.linewidth(0.06)])
-            self.c.text(x + 1.1, y, pre.capitalize() + ' ' + name.capitalize(), [text.parbox(4), text.halign.boxleft, text.halign.flushleft, text.valign.middle, color.gray(0.3)])
+            y =  - self.s * (self.r + 3 - (j/2)*2)
+            self.c.stroke(
+                path.path(
+                    path.moveto(x,y),
+                    # Voeg relatieve lijn toe 
+                    path.rlineto(1, 0)
+                ), 
+                [color.rgbfromhexstring(layer['color']), style.linewidth(0.06)]
+            )
+            self.c.text(
+                x + 1.1, 
+                y, 
+                # Legende titel
+                layer['name'].capitalize(), 
+                # Stijl van de tekst
+                [text.parbox(4), text.halign.boxleft, text.halign.flushleft, text.valign.middle, color.gray(0.3)]
+            )
+            # Verhoog de index
+            j += 1
 
-    def save(self, name='test'):
+    def save(self, name='test', legend=False):
         # Schrijf een PDF file weg
-        self.generateCanvas()
-        self.c.writePDFfile(os.path.join('src', name))
+        self.generateRadarPlot()
+        # Voeg legende toe
+        if legend == True:
+            self.addLegend()
+        # Schrijf het weg naar een .svg bestand
+        self.c.writeSVGfile(os.path.join('export', name))
 
 
-# In[2]: Load the data
+# In[2]: Laad de data
 
 df = pd.read_excel('20200214kernstudie.xlsx')
 
-# In[3]: Loop over alle provincies
+# In[3]: Normaliseer de data
+
+df[df.columns[1:]] = df[df.columns[1:]] / df[df.columns[1:]].max() * 5
+
+# In[4]: Loop over alle indices
+
+# Groepeer eventuele duplicaten
 ids = df.groupby('ID')
 for i, data in ids:
-    if i > 2:
-        exit
+    # TIJDENS DEBUGGEN - VERWIJDER IN PRODUCTIE
+    if i > 0:
+        break
     # Loop over alle waardes
-    plot = RadarPlot()
-    plot.addLayer(data.iloc[:,1:], name='kernstudie', color='#B1DFD5')
-    plot.save(f'{i}')
-
-# %%
+    plot = RadarPlot(items=5,gridlines=5)
+    # De data die je aan de lagen toevoegd is van het type
+    # Pandas DataSeries en dus geen DataFrame!
+    plot.addLayer(data.iloc[0,1:], name='kernstudie')
+    # Bewaar de plot met als bestandsnaam het ID-nummer
+    # Standaard wordt de legende verborgen
+    plot.save(f'{i}', legend=True)
